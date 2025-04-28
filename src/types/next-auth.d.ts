db9046e70b26190001d8d3590a039f531886ac8c
eufiguei <erik.ubing@gmail.com>
2025-04-28 20:00:32 -0300

    changes

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id: string;
+      tipo: string;
+    };
+  }
+
+  interface User extends DefaultUser {
+    tipo: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    tipo: string;
+  }
+}
