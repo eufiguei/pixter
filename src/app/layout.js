fc35c11abe6aee2341c5ff4c4d03682c31226e2b
eufiguei <erik.ubing@gmail.com>
2025-04-28 01:19:05 -0300

    changes3

---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+import { AuthProvider } from '@/lib/auth/session';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
