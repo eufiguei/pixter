3ee79402a1c6959eddd07e586d29dcdb1c129f30
eufiguei <erik.ubing@gmail.com>
2025-05-01 18:11:00 -0300

    ouuu

--- src/app/api/client/payment-methods/[id]/route.ts
+++ src/app/api/client/payment-methods/[id]/route.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16', // Use a recent stable API version
+  apiVersion: '2022-11-15', // Use a recent stable API version
 });
 
 export async function DELETE(
